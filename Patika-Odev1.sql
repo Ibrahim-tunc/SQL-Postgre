5b238cfbbbac20696b8b65310ba527979f8af26d
Ibrahim-tunc <46247396+Ibrahim-tunc@users.noreply.github.com>
2022-01-02 18:20:05 +0300

    Create Patika-Odev1.sql

---
+++ Patika-Odev1.sql
@@ -0,0 +1,14 @@
+-- SELECT title, description FROM film;
+
+-- SELECT * FROM film 
+WHERE length > 60 AND length < 75 ;
+
+-- SELECT * FROM film 
+WHERE rental_rate = 0.99 AND ( replacement_cost =  12.99 OR
+replacement_cost = 28.99 )
+
+--select last_name from customer 
+where first_name = 'Mary'
+
+--select * from film
+where not (length > 50) and not (rental_rate = 2.99 or rental_rate = 4.99)
